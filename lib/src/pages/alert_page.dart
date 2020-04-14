81f36ec0ae9bf40cc4cb4b29a4f030671f64c58f
Anthony Rozon <arozonm@gmail.com>
2020-04-14 10:47:32 -0400

    Added demo dialog alert

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -13,6 +13,43 @@ class AlertPage extends StatelessWidget {
           Navigator.pop(context);
         },
       ),
+      body: Center(
+        child: FlatButton(
+          onPressed: () => _showAlert(context),
+          child: Text("Show Alert"),
+          color: Colors.blue,
+          textColor: Colors.white,
+        ),
+      ),
     );
+  }
+
+  _showAlert(BuildContext context) {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)),
+            title: Text("Alert Dialog Demo"),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text(
+                    "This is the content of the Alert Dialog demo. This shouldn't take too much space. Flutter is awesome."),
+                FlutterLogo(size: 100.0)
+              ],
+            ),
+            actions: <Widget>[
+              FlatButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Text("Cancel")),
+              FlatButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Text("Ok")),
+            ],
+          );
+        });
   }
 }
