21f44dadecedbb31f12cbeced355b9d469199b34
Anthony Rozon <arozonm@gmail.com>
2020-04-19 14:55:07 -0400

    Added AnimatedContainer page demo

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -4,12 +4,14 @@ import 'package:components_app/src/pages/home_page.dart';
 import 'package:components_app/src/pages/alert_page.dart';
 import 'package:components_app/src/pages/avatar_page.dart';
 import 'package:components_app/src/pages/card_page.dart';
+import 'package:components_app/src/pages/animated_container_page.dart';
 
 Map<String, WidgetBuilder> getNavigationRoutes() {
   return <String, WidgetBuilder>{
     '/': (BuildContext context) => HomePage(),
     'alert': (BuildContext context) => AlertPage(),
     'avatar': (BuildContext context) => AvatarPage(),
-    'card': (BuildContext context) => CardPage()
+    'card': (BuildContext context) => CardPage(),
+    'animatedContainer': (BuildContext context) => AnimatedContainerPage()
   };
 }
