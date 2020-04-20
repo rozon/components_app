1bb39aefd694057d06f3800b66e4970c6e8ca7b1
Anthony Rozon <arozonm@gmail.com>
2020-04-20 17:42:59 -0400

    Added slider and checkbox demo page

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -6,6 +6,7 @@ import 'package:components_app/src/pages/avatar_page.dart';
 import 'package:components_app/src/pages/card_page.dart';
 import 'package:components_app/src/pages/animated_container_page.dart';
 import 'package:components_app/src/pages/input_page.dart';
+import 'package:components_app/src/pages/slider_page.dart';
 
 Map<String, WidgetBuilder> getNavigationRoutes() {
   return <String, WidgetBuilder>{
@@ -15,5 +16,6 @@ Map<String, WidgetBuilder> getNavigationRoutes() {
     'card': (BuildContext context) => CardPage(),
     'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
     'inputs': (BuildContext context) => InputPage(),
+    'slider': (BuildContext context) => SliderPage(),
   };
 }
